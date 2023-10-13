e02cbcf8fc25e3dd897299986c8c5885dfd74e41
unknown <you@example.com>
2023-10-13 23:57:20 +0200

    Initial commit: created base project structure, added BaseEntity class and interface for generic repo

---
+++ GymApp.Application/Interfaces/Persistence/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using GymApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Application.Interfaces.Persistence
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+    }
+
+}
---
+++ GymApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Domain.Common
+{
+    public abstract class BaseEntity
+    {  
+        public Guid Id { get; set; }
+        public DateTime? CreatedOn { get; set;}
+        public DateTime? UpdatedOn { get; set;}
+    }
+}
